fbd643200cdd5eecc2ea65795db39bfd4ace9f22
AkashSinha007 <akashkumarsinha007@gmail.com>
2024-02-04 11:43:37 +0100

    Added Database Access through knex query builder over sqlite3

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,18 +1,7 @@
+import { getJobs } from './db/jobs.js'
+
 export const resolvers = {
     Query: {
-        jobs: () => {
-            return [
-            {
-                id: 'test-id-1',
-                title: 'The title 1',
-                description: 'The description',
-            },
-            {
-                id: 'test-id-2',
-                title: 'The title 2',
-                description: 'The description',
-            },
-        ];
+        jobs: () => getJobs(),
     },
-    }
 };
